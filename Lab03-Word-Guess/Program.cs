566dff49574a7102d74783f4d7d29cb79f8702d6
Kent Ketter <kent.ketter1@gmail.com>
2019-01-13 19:19:23 -0800

    game functions working

--- Lab03-Word-Guess/Program.cs
+++ Lab03-Word-Guess/Program.cs
@@ -5,7 +5,7 @@ namespace Lab03_Word_Guess
 {
     class Program
     {
-        public static string guessPath = "../../../../guessFile";
+        public static string guessPath = "../../../../guessFile.txt";
         public static string path = "../../../../wordFile.txt";
         public static bool isCorrect = false;
         public static string guessThisWord;
@@ -30,7 +30,7 @@ namespace Lab03_Word_Guess
                 case 1:
                     CreateFile();
                     CreateFileGuesses();
-                    //Game();
+                    Game();
                     break;
                 case 2:
                     AdminMenu();
@@ -78,7 +78,7 @@ namespace Lab03_Word_Guess
             }
         }
 
-        // admin menu functions - application of CRUD
+        // admin menu functions
         /// <summary>
         /// Display all words in the word file
         /// </summary>
@@ -101,9 +101,9 @@ namespace Lab03_Word_Guess
             Console.WriteLine();
             AdminMenu();
         }
-       /// <summary>
-       /// prompts user to delete word from bank, checks word against file, deletes and re-writes file w/o selected word
-       /// </summary>
+        /// <summary>
+        /// prompts user to delete word from bank, checks word against file, deletes and re-writes file w/o selected word
+        /// </summary>
         public static void RemoveOneWord()
         {
             string[] words = File.ReadAllLines(path);
@@ -139,7 +139,11 @@ namespace Lab03_Word_Guess
             ViewWords();
             AdminMenu();
         }
-
+        /// <summary>
+        /// converts pick strings into integers, contains exception for wrong menu inputs
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
         public static int PickHandler(string userInput)
         {
             int pick;
@@ -156,6 +160,117 @@ namespace Lab03_Word_Guess
             pick = Convert.ToInt32(userInput);
             return pick;
         }
+
+        /// <summary>
+        /// selects a random word from the words file to begin the game
+        /// </summary>
+        public static void Game()
+        {
+            guessThisWord = RandomWord(path);
+            while(isCorrect == false)
+            {
+                Console.WriteLine(String.Join(' ', WordChecker(guessThisWord)));
+                ReadFile(guessPath);
+                GuessConversion(GuessPrompt(), guessThisWord);
+                Console.WriteLine();
+            }
+            Console.WriteLine(String.Join(' ', WordChecker(guessThisWord)));
+            Console.WriteLine("You Won!");
+            isCorrect = false;
+            DeleteFile(guessPath);
+            DeleteFile(path);
+            Menu();
+        }
+        /// <summary>
+        /// Selects a random word from the words file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string RandomWord(string path)
+        {
+            string[] words = File.ReadAllLines(path);
+            int wordsLength = words.Length;
+            Random r = new Random();
+            int whichWord = r.Next(wordsLength);
+            string word = words[whichWord];
+            return word;
+        }
+        /// <summary>
+        /// Creates blanks ie word form for guessing, checks guesses against targeted random word, for correct guesses fills in word for correspoding blank
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static char[] WordChecker(string word)
+        {
+            int wordLength = word.Length;
+            char[] wordForm = new char[wordLength];
+            for (int i = 0; i < wordLength; i++)
+            {
+                wordForm[i] = '_';
+            }
+
+            string[] guessedLetters = File.ReadAllLines(guessPath);
+            char[] guessedLettersChar = new char[guessedLetters.Length];
+            for (int i = 1; i < guessedLetters.Length; i++)
+            {
+                guessedLettersChar[i - 1] = Convert.ToChar(guessedLetters[i]);
+            };
+            for (int i = 0; i < guessedLetters.Length; i++)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if(word[j] == guessedLettersChar[i])
+                    {
+                        wordForm[j] = word[j];
+                    }
+                }
+            }
+            return wordForm;
+        }
+        /// <summary>
+        /// converts guess from string to char and handles conversion exceptions, these guesses are appended to a guess file, lastly value is checked for "correct-ness"
+        /// </summary>
+        /// <param name="letterInput"></param>
+        /// <param name="word"></param>
+        public static void GuessConversion(string letterInput, string word)
+        {
+            char letter;
+            bool canConvert = false;
+            while (canConvert == false)
+            {
+                try
+                {
+                    letter = Convert.ToChar(letterInput);
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Only enter one letter to guess!");
+                    letterInput = GuessPrompt();
+                }
+                canConvert = true;
+            }
+            letter = Convert.ToChar(letterInput);
+            AppendToFile(guessPath, letterInput);
+            char[] checkedWord = WordChecker(word);
+            string checkedWordStr = new string(checkedWord);
+            if (checkedWordStr == word)
+            {
+                isCorrect = true;
+            }
+        }
+        /// <summary>
+        /// Prompts user to guess
+        /// </summary>
+        /// <returns></returns>
+        public static string GuessPrompt()
+        {
+            Console.Write("Guess a letter: ");
+            string choice = Console.ReadLine();
+            string choiceStr = choice.ToUpper();
+            return choiceStr;
+        }
+
         public static void CreateFileGuesses()
         {
             using (StreamWriter streamWriter = new StreamWriter(guessPath))
@@ -163,16 +278,14 @@ namespace Lab03_Word_Guess
                 streamWriter.WriteLine("Guesses");
             }
         }
-
-        // CRUD functions
         public static void CreateFile()
         {
             using (StreamWriter streamWriter = new StreamWriter(path))
             {
-                streamWriter.WriteLine("Karn");
-                streamWriter.WriteLine("Tezzeret");
-                streamWriter.WriteLine("Bolas");
-                streamWriter.WriteLine("Chandra");
+                streamWriter.WriteLine("KARN");
+                streamWriter.WriteLine("TEZZERET");
+                streamWriter.WriteLine("GARRUK");
+                streamWriter.WriteLine("CHANDRA");
             }
 
         }
